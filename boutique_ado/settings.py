4df5c26bdcee504af0620dbb8b864635dbd55ff7
Arcandrus <eric.harper87@gmail.com>
2025-06-26 15:24:11 +0100

    Updated AWS Object Parameters

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -189,6 +189,11 @@ STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94600000',
+    }
+    
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-alpha'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
